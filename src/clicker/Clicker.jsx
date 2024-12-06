9fdef08850dd3884796d81c97aec10582681a51a
Josse DE OLIVEIRA <contactme@webjosse.fr>
2024-12-06 02:06:07 +0100

    click anim fixed

--- src/clicker/Clicker.jsx
+++ src/clicker/Clicker.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from 'react';
 import co2CloudImage from '../assets/co2-cloud.png';
 
 export const Clicker = ({ onClick }) => {
+
+    const [clicked, setClicked] = useState(false);
+
+    useEffect(() => {
+        setClicked(false)
+    }, [clicked])
+
+    function clickAnimAndOn(){
+        setClicked(true);
+        onClick();
+    }
+
     return (
       <div className='clickoverfl'>
         <img
           src={co2CloudImage}
           alt="CO2 Cloud"
-          onClick={onClick}
+          onClick={clickAnimAndOn}
           style={{ cursor: 'pointer' }}
-          className='clicker-image'
+          className={`clicker-image ${clicked?' clicked-clicker':''}`}
         />
       </div>
     );
